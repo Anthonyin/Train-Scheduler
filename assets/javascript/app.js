999dde34cc39a8f595cd77db1204bd74497b7cde
Anthony Yin <anthony923x@gmail.com>
2018-07-26 00:50:18 -0400

    finish coding, not working yet

---
+++ assets/javascript/app.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+  var config = {
+    apiKey: 'AIzaSyBYSHdlrtu4dFwywxv10KVJ-Z7UeyVJtZ0',
+    authDomain: 'train-scheduler-ytf.firebaseapp.com',
+    databaseURL: 'https://train-scheduler-ytf.firebaseio.com',
+    projectId: 'train-scheduler-ytf',
+    storageBucket: '',
+    messagingSenderId: '416433187430'
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  // var newName;
+  // var newDestination;
+  // var newFirstTrain;
+  // var newFrequency = 0;
+
+  $('#newTrainName').on('click', function(event) {
+    event.preventDefault();
+    var currName = $('#newTrainName')
+      .val()
+      .trim();
+    var currDestination = $('#newTrainDestination')
+      .val()
+      .trim();
+    var currFirstTrain = $('#newTrainTime')
+      .val()
+      .trim();
+    var currFrequency = $('#frequency')
+      .val()
+      .trim();
+    var newTrain = {
+      name: currName,
+      destination: currDestination,
+      firstTrain: currFirstTrain,
+      frequency: currFrequency
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+
+    $('#newTrainName').val('');
+    $('#newTrainDestination').val('');
+    $('#newTrainTime').val('');
+    $('#frequency').val('');
+  });
+
+  database.ref().on('child_added', function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var currName = childSnapshot.val().name;
+    var currDestination = childSnapshot.val().destination;
+    var currFirstTrain = childSnapshot.val().firstTrain;
+    var currFrequency = childSnapshot.val().frequency;
+
+    console.log(currName);
+    console.log(currDestination);
+    console.log(currFirstTrain);
+    console.log(currFrequency);
+
+    var nextArr;
+    var minAway;
+
+    var firstTrainNew = moment(
+      childSnapshot.val().firstTrain,
+      'hh;mm'
+    ).subtract(1, 'years');
+    var diffTime = moment().diff(moment(firstTrainNew), 'tinutes');
+    var remainder = diffTime % childSnapshot.val().frequency;
+    var minAway = childSnapshot.val().frequency - remainder;
+    var nextTrain = moment(nextTrain).format('hh:mm');
+
+    var newRow = $('<tr>').append(
+      $('<td>').text(currName),
+      $('<td>').text(currDestination),
+      $('<td>').text(currFirstTrain),
+      $('<td>').text(currFrequency)
+    );
+
+    $('#add-row').append(newRow);
+  });
+});
